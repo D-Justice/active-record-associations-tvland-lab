c0a8537570487adb73b711d4d564ad2e7474444a
D-Justice <justice.damian96@gmail.com>
2021-11-23 09:02:50 +1100

    Assignment Complete

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+    def list_roles
+        first_name = self.characters.map do |each|
+            each.name
+        end.flatten
+        show_name = self.shows.map do |each|
+            each.name
+        end.flatten
+        first_name[0] + ' - ' + show_name[0]
+    end
 end
